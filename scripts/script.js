81b0bf9eb4f8b15cbf6c579c0f5b518c2a014f0a
Bruno Codinha <brunocodinha@Brunos-MBP.home>
2023-05-19 15:41:34 +0100

    script upgrade

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,6 @@
 // check if window is scrolled to add opacity to the navbar
 window.addEventListener('scroll', function() {
     var navbar = document.querySelector('.navbar');
-    var scrolled = window.scrollY > 0;
+    var scrolled = window.scrollY > 10;
     navbar.classList.toggle('navbar-scrolled', scrolled);
   });
