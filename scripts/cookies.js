baf0720f2ce3e3dc6a76f473c3f3dd4b47dabe79
tiagocodinha <98787370+tiagocodinha@users.noreply.github.com>
2024-12-17 02:52:24 +0000

    cookies

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -1,3 +1,14 @@
+function acceptCookies() {
+  // Atualiza o consentimento do Google
+  gtag('consent', 'update', {
+      'ad_storage': 'granted',
+      'analytics_storage': 'granted'
+  });
+
+  // Oculta o banner
+  document.getElementById('cookie-banner').style.display = 'none';
+}
+
 if (!localStorage.getItem('cookies-accepted')) {
   document.getElementById('cookie-banner').style.display = 'block';
 }
@@ -7,3 +18,4 @@ function acceptCookies() {
   localStorage.setItem('cookies-accepted', 'true');
   document.getElementById('cookie-banner').style.display = 'none'; // Ocultar o banner
 }
+
